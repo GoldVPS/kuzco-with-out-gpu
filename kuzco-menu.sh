a44b754877f8e779b302db80ac8d04163e26c88d
GoldVPS <goldvpsbymiftaikyy@gmail.com>
2025-07-10 04:54:45 +0700

    Update kuzco-menu.sh

--- kuzco-menu.sh
+++ kuzco-menu.sh
@@ -3,28 +3,8 @@
 # Warna terminal
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
-RED='\033[0;31m'
 RESET='\033[0m'
 BLUE_LINE="\033[1;34m────────────────────────────────────────────${RESET}"
-
-WORKERS_DIR=~/kuzco-workers
-
-mkdir -p "$WORKERS_DIR"
-
-function list_workers() {
-    echo -e "${CYAN}Available Workers:${RESET}"
-    if [ "$(ls -A $WORKERS_DIR)" ]; then
-        i=1
-        for dir in "$WORKERS_DIR"/*; do
-            if [ -d "$dir" ]; then
-                echo "  $i) $(basename "$dir")"
-                ((i++))
-            fi
-        done
-    else
-        echo -e "${RED}  No workers found.${RESET}"
-    fi
-}
 
 while true; do
     clear
@@ -41,16 +21,6 @@ while true; do
 
     case $option in
         1)
-            echo -e "${CYAN}Enter a unique Worker Name (no spaces):${RESET}"
-            read worker_name
-            worker_path="$WORKERS_DIR/$worker_name"
-
-            if [ -d "$worker_path" ]; then
-                echo -e "${RED}Worker name already exists. Choose a different name.${RESET}"
-                sleep 2
-                continue
-            fi
-
             echo -e "${CYAN}Enter Vikey API Key:${RESET}"
             read api_key
             echo -e "${CYAN}Enter Kuzco Worker Code:${RESET}"
@@ -61,65 +31,31 @@ while true; do
             apt install -y docker.io docker-compose git
 
             echo -e "${GREEN}Cloning installer...${RESET}"
-            git clone https://github.com/direkturcrypto/kuzco-installer-docker "$worker_path"
+            rm -rf ~/kuzco-installer-docker
+            git clone https://github.com/direkturcrypto/kuzco-installer-docker ~/kuzco-installer-docker
 
-            cd "$worker_path/kuzco-main"
+            cd ~/kuzco-installer-docker/kuzco-main
             sed -i "s/YOUR_VIKEY_API_KEY/$api_key/" docker-compose.yml
             sed -i "s/YOUR_WORKER_CODE/$worker_code/" docker-compose.yml
 
-            echo -e "${GREEN}Starting worker '$worker_name'...${RESET}"
+            echo -e "${GREEN}Starting worker...${RESET}"
             docker-compose up -d --build
             read -n 1 -s -r -p "Press any key to return to menu"
             ;;
         2)
-            list_workers
-            echo -ne "${CYAN}Enter the number of the worker to view logs:${RESET} "
-            read choice
-
-            selected=$(ls -d $WORKERS_DIR/* | sed -n "${choice}p")
-
-            if [ -z "$selected" ]; then
-                echo -e "${RED}Invalid choice.${RESET}"
-                sleep 1
-                continue
-            fi
-
-            cd "$selected/kuzco-main" || exit
+            cd ~/kuzco-installer-docker/kuzco-main || exit
             docker-compose logs -f --tail 100
             ;;
         3)
-            list_workers
-            echo -ne "${CYAN}Enter the number of the worker to stop:${RESET} "
-            read choice
-
-            selected=$(ls -d $WORKERS_DIR/* | sed -n "${choice}p")
-
-            if [ -z "$selected" ]; then
-                echo -e "${RED}Invalid choice.${RESET}"
-                sleep 1
-                continue
-            fi
-
-            cd "$selected/kuzco-main" || exit
+            cd ~/kuzco-installer-docker/kuzco-main || exit
             docker-compose down
             echo -e "${GREEN}Worker stopped.${RESET}"
             sleep 1
             ;;
         4)
-            list_workers
-            echo -ne "${CYAN}Enter the number of the worker to reinstall:${RESET} "
-            read choice
-
-            selected=$(ls -d $WORKERS_DIR/* | sed -n "${choice}p")
-
-            if [ -z "$selected" ]; then
-                echo -e "${RED}Invalid choice.${RESET}"
-                sleep 1
-                continue
-            fi
-
-            rm -rf "$selected"
-            echo -e "${GREEN}Worker removed. Run option 1 to reinstall.${RESET}"
+            echo -e "${GREEN}Reinstalling worker...${RESET}"
+            rm -rf ~/kuzco-installer-docker
+            echo -e "${CYAN}Worker removed. Run option 1 to reinstall.${RESET}"
             sleep 1
             ;;
         5)
